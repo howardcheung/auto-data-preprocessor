0e4d34601f5fe9346efac0c81e6cbd363507e2cf
howardcheung <howard.at@gmail.com>
2017-04-12 22:38:20 +0800

    Add Option to Change the Initial Value
    Add option to change the initial value at the starting
    time if it extrapolates

--- src/format_data.py
+++ src/format_data.py
@@ -21,7 +21,7 @@ from pandas import DataFrame
 # write functions
 def convert_df(datadf: DataFrame, start_time: datetime,
                end_time: datetime=None, interval: float=600,
-               step: bool=True) -> DataFrame:
+               step: bool=True, ini_val: int=1) -> DataFrame:
     """
         This function converts a dataframe which data are converted according
         to time of change of values to data collected at fixed intervals.
@@ -48,6 +48,13 @@ def convert_df(datadf: DataFrame, start_time: datetime,
         step: bool
             if the data should be considered to be step functions. Default
             True. Indeed this input won't work until further notice.
+
+        ini_val: int
+            the assumption to the initial value of a column if the start time
+            is before the occurrence of the initial value in the column.
+                1: Use the minimum value in the trend
+                2: Use the first value in the trend
+            Default 1
     """
 
     # calculate the ending index for the new dataframe
@@ -75,15 +82,32 @@ def convert_df(datadf: DataFrame, start_time: datetime,
                 if not isnan(datadf.loc[oldind, col]):
                     pos = oldind
                     break
-            if final_df.index[0] >= pos:
+            if final_df.index[0] >= pos or ini_val == 2:
                 final_df.loc[final_df.index[0], col] = datadf.loc[pos, col]
             else:
                 final_df.loc[final_df.index[0], col] = \
                     datadf[col].dropna().unique().min()
 
         # continue to append new columns until the end
-
-        pass
+        oldind = 0
+        newind = 1
+        oldlen = datadf.shape[0]
+        newlen = final_df.shape[0]
+        while newind < newlen:
+            if final_df.index[newind] < datadf.index[oldind]:
+                final_df.loc[final_df.index[newind], :] = \
+                    final_df.loc[final_df.index[newind-1], :]
+            else:
+                for col in final_df.columns:
+                    if isnan(datadf.loc[datadf.index[oldind], col]):
+                        final_df.loc[final_df.index[newind], col] = \
+                            final_df.loc[final_df.index[newind-1], col]
+                    else:
+                        final_df.loc[final_df.index[newind], col] = \
+                            datadf.loc[datadf.index[oldind], col]
+                if oldind < oldlen-1:
+                    oldind += 1
+            newind += 1
     else:
         raise ValueError(u''.join([
             u'The interpolation function of format_data.convert_df is',
@@ -102,18 +126,33 @@ if __name__ == '__main__':
     # check function for computer-generated ending time
     FILENAME = '../dat/time_of_change.csv'
     TEST_DF = read_data(FILENAME, header=0)
-    NEW_DF = convert_df(TEST_DF, datetime(2011, 1, 11, 0, 0))
+    NEW_DF = convert_df(TEST_DF, datetime(2011, 1, 1, 0, 0))
     assert isinstance(NEW_DF, DataFrame)
     assert NEW_DF.index[-1] <= TEST_DF.index[-1]
     assert NEW_DF.index[-1] >= TEST_DF.index[-2]
 
     # check function for new ending time
     NEW_DF = convert_df(
-        TEST_DF, datetime(2011, 1, 1, 0, 0), datetime(2011, 12, 31, 11, 50)
+        TEST_DF, datetime(2011, 1, 1, 0, 0), datetime(2011, 1, 3, 11, 50)
     )
-    print(NEW_DF)
-    assert NEW_DF.index[-1] == datetime(2011, 12, 31, 11, 50)
+    assert NEW_DF.index[-1] == datetime(2011, 1, 3, 11, 50)
     assert NEW_DF.loc[NEW_DF.index[0], 'Item 4'] == 0.0
     assert NEW_DF.loc[NEW_DF.index[0], 'Item 3'] == 0.0
+    # check function of the correction
+    assert NEW_DF.loc[datetime(2011, 1, 3, 10, 50), 'Item 1'] == 0.0
+    assert NEW_DF.loc[datetime(2011, 1, 3, 10, 50), 'Item 2'] == 1.0
+    assert NEW_DF.loc[datetime(2011, 1, 3, 10, 50), 'Item 3'] == 0.0
+    assert NEW_DF.loc[datetime(2011, 1, 3, 10, 50), 'Item 4'] == 0.0
 
+    # check function with new initial values    
+    NEW_DF = convert_df(
+        TEST_DF, datetime(2011, 1, 1, 0, 0), datetime(2011, 1, 3, 11, 50),
+        ini_val=2
+    )
+    # check function of the correction
+    assert NEW_DF.loc[datetime(2011, 1, 1, 0, 0), 'Item 1'] == 1.0
+    assert NEW_DF.loc[datetime(2011, 1, 1, 0, 0), 'Item 2'] == 1.0
+    assert NEW_DF.loc[datetime(2011, 1, 1, 0, 0), 'Item 3'] == 1.0
+    assert NEW_DF.loc[datetime(2011, 1, 1, 0, 0), 'Item 4'] == 0.0
+    
     print('All functions in', basename(__file__), 'are ok')
