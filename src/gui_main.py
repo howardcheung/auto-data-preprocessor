f6c47d36b9229a54cd33b198f29efc708bab6344
Howard Cheung <tungeast@protonmail.com>
2017-04-13 18:29:50 +0800

    Do not use GridBadSizer for the start time indicators

--- src/gui_main.py
+++ src/gui_main.py
@@ -51,104 +51,110 @@ class MainGUI(wx.Frame):
         # define the panel
         panel = wx.Panel(self)
 
-        sizer = wx.GridBagSizer(1, 1)  # making a grid in your box
+        # sizer = wx.GridBagSizer(1, 1)  # making a grid in your box
 
         # title
         # leave space at the top, left and bottom from the text to the
         # other object
-        sizer.Add(
-            wx.StaticText(panel, label=u''.join([
-                u'Time-of-change value data to data\n',
-                u'at constant time intervals'
-            ])),
-            pos=(0, 0),  # position: (from top to bottom, from left to right)
-            flag=wx.TOP | wx.LEFT | wx.BOTTOM,
-            border=10  # border required for flag indicators
-        )
+        
+        # position: (from top to bottom, from left to right)
+        wx.StaticText(panel, label=u''.join([
+            u'Time-of-change value data to data',
+            u'at constant time intervals'
+        ]), pos=(20, 20))
+        # sizer.Add(
+            # wx.StaticText(panel, label=u''.join([
+                # u'Time-of-change value data to data\n',
+                # u'at constant time intervals'
+            # ])),
+            # pos=(0, 0),  # position: (from top to bottom, from left to right)
+            # flag=wx.TOP | wx.LEFT | wx.BOTTOM,
+            # border=10  # border required for flag indicators
+        # )
 
-        # Inputs to the data file path
-        text = wx.StaticText(panel, label=u'Data file path:')
-        sizer.Add(
-            text, pos=(1, 0), flag=wx.TOP | wx.LEFT | wx.BOTTOM, border=10
-        )
+        # # Inputs to the data file path
+        # text = wx.StaticText(panel, label=u'Data file path:')
+        # sizer.Add(
+            # text, pos=(1, 0), flag=wx.TOP | wx.LEFT | wx.BOTTOM, border=10
+        # )
 
-        # require additional object for textbox
-        # with default path
-        self.dfpath = wx.TextCtrl(panel, value=u'../dat/time_of_change.csv')
-        sizer.Add(
-            self.dfpath, pos=(1, 1), span=(1, 3), flag=wx.TOP | wx.EXPAND,
-            border=10
-        )
-        button = wx.Button(panel, label=u'Browse...')
-        button.Bind(wx.EVT_BUTTON, self.OnOpen)
-        sizer.Add(button, pos=(1, 4), flag=wx.TOP | wx.RIGHT, border=10)
+        # # require additional object for textbox
+        # # with default path
+        # self.dfpath = wx.TextCtrl(panel, value=u'../dat/time_of_change.csv')
+        # sizer.Add(
+            # self.dfpath, pos=(1, 1), span=(1, 3), flag=wx.TOP | wx.EXPAND,
+            # border=10
+        # )
+        # button = wx.Button(panel, label=u'Browse...')
+        # button.Bind(wx.EVT_BUTTON, self.OnOpen)
+        # sizer.Add(button, pos=(1, 4), flag=wx.TOP | wx.RIGHT, border=10)
 
-        # ask for existence of header as a checkbox
-        text = wx.StaticText(panel, label=u'Existence of a header row:')
-        sizer.Add(
-            text, pos=(2, 0), flag=wx.TOP | wx.LEFT | wx.BOTTOM, border=10
-        )
-        self.header = wx.CheckBox(
-            panel, pos=(20, 20)
-        )
-        sizer.Add(self.header, pos=(2, 1), flag=wx.TOP | wx.RIGHT, border=10)
+        # # ask for existence of header as a checkbox
+        # text = wx.StaticText(panel, label=u'Existence of a header row:')
+        # sizer.Add(
+            # text, pos=(2, 0), flag=wx.TOP | wx.LEFT | wx.BOTTOM, border=10
+        # )
+        # self.header = wx.CheckBox(
+            # panel, pos=(20, 20)
+        # )
+        # sizer.Add(self.header, pos=(2, 1), flag=wx.TOP | wx.RIGHT, border=10)
 
-        # Inputs to the directory to save the plots
-        text = wx.StaticText(panel, label=u'Path to save file:')
-        sizer.Add(
-            text, pos=(3, 0), flag=wx.TOP | wx.LEFT | wx.BOTTOM, border=10
-        )
+        # # Inputs to the directory to save the plots
+        # text = wx.StaticText(panel, label=u'Path to save file:')
+        # sizer.Add(
+            # text, pos=(3, 0), flag=wx.TOP | wx.LEFT | wx.BOTTOM, border=10
+        # )
 
-        # require additional object for textbox
-        # with default path
-        self.newdfpath = wx.TextCtrl(panel, value=u'../testplots/example.csv')
-        sizer.Add(
-            self.newdfpath, pos=(3, 1), span=(1, 3), flag=wx.TOP | wx.EXPAND,
-            border=10
-        )
-        button = wx.Button(panel, label=u'Browse...')
-        button.Bind(wx.EVT_BUTTON, self.SaveOpen)
-        sizer.Add(button, pos=(3, 4), flag=wx.TOP | wx.RIGHT, border=10)
+        # # require additional object for textbox
+        # # with default path
+        # self.newdfpath = wx.TextCtrl(panel, value=u'../testplots/example.csv')
+        # sizer.Add(
+            # self.newdfpath, pos=(3, 1), span=(1, 3), flag=wx.TOP | wx.EXPAND,
+            # border=10
+        # )
+        # button = wx.Button(panel, label=u'Browse...')
+        # button.Bind(wx.EVT_BUTTON, self.SaveOpen)
+        # sizer.Add(button, pos=(3, 4), flag=wx.TOP | wx.RIGHT, border=10)
 
-        # Inputs to the format time string
-        text = wx.StaticText(panel, label=u''.join([
-            u'Format time string in the first column:'
-        ]))
-        sizer.Add(
-            text, pos=(4, 0), flag=wx.TOP | wx.LEFT | wx.BOTTOM, border=10
-        )
+        # # Inputs to the format time string
+        # text = wx.StaticText(panel, label=u''.join([
+            # u'Format time string in the first column:'
+        # ]))
+        # sizer.Add(
+            # text, pos=(4, 0), flag=wx.TOP | wx.LEFT | wx.BOTTOM, border=10
+        # )
 
-        # require additional object for textbox
-        self.timestring = wx.TextCtrl(panel, value=u'%m/%d/%y %I:%M:%S %p CST')
-        sizer.Add(
-            self.timestring, pos=(4, 1), span=(1, 3), flag=wx.TOP | wx.EXPAND,
-            border=10
-        )
+        # # require additional object for textbox
+        # self.timestring = wx.TextCtrl(panel, value=u'%m/%d/%y %I:%M:%S %p CST')
+        # sizer.Add(
+            # self.timestring, pos=(4, 1), span=(1, 3), flag=wx.TOP | wx.EXPAND,
+            # border=10
+        # )
 
-        # a button for instructions
-        button = wx.Button(
-            panel,
-            label=u'Instructions to enter the format of the time string'
-        )
-        button.Bind(wx.EVT_BUTTON, self.TimeInstruct)
-        sizer.Add(
-            button, pos=(5, 1), span=(1, 2)
-        )
+        # # a button for instructions
+        # button = wx.Button(
+            # panel,
+            # label=u'Instructions to enter the format of the time string'
+        # )
+        # button.Bind(wx.EVT_BUTTON, self.TimeInstruct)
+        # sizer.Add(
+            # button, pos=(5, 1), span=(1, 2)
+        # )
 
-        # add start time check boxes
-        text = wx.StaticText(panel, label=u''.join([
-            u'Start time in the new data file:'
-        ]))
-        sizer.Add(
-            text, pos=(6, 0), flag=wx.TOP | wx.LEFT | wx.BOTTOM, border=10
-        )
+        # # add start time check boxes
+        # text = wx.StaticText(panel, label=u''.join([
+            # u'Start time in the new data file:'
+        # ]))
+        # sizer.Add(
+            # text, pos=(6, 0), flag=wx.TOP | wx.LEFT | wx.BOTTOM, border=10
+        # )
 
-        # create check boxes for the date and time
-        self.start_yr = wx.SpinCtrl(panel, value='2016', min=1, max=4000)
-        sizer.Add(
-            self.start_yr, pos=(6, 1), flag=wx.TOP | wx.EXPAND,
-            border=10
-        )
+        # # create check boxes for the date and time
+        # self.start_yr = wx.SpinCtrl(panel, value='2016', min=1, max=4000)
+        # sizer.Add(
+            # self.start_yr, pos=(6, 1), flag=wx.TOP | wx.EXPAND,
+            # border=10
+        # )
 
         # buttons at the bottom
         # button_ok = wx.Button(panel, label=u'Analysis')
@@ -156,9 +162,9 @@ class MainGUI(wx.Frame):
         # sizer.Add(button_ok, pos=(8, 4))
 
         # sizing of the window
-        sizer.AddGrowableCol(2)  # expand to meet the window?
+        # sizer.AddGrowableCol(2)  # expand to meet the window?
 
-        panel.SetSizer(sizer)
+        # panel.SetSizer(sizer)
 
     def ShowMessage(self):
         """
