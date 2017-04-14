7d5b5666a9a2998b028e717147ccb37fe5e73db4
howardcheung <howard.at@gmail.com>
2017-04-14 10:53:12 +0800

    Removing dependence on numpy for license issue

--- src/data_read.py
+++ src/data_read.py
@@ -13,7 +13,7 @@ from datetime import datetime
 from math import isnan
 
 # import third party libraries
-from numpy import where
+# from numpy import where
 from pandas import DataFrame, Series, ExcelFile, read_csv, read_excel
 from pandas.tslib import Timestamp
 
@@ -86,17 +86,18 @@ def read_data(filename: str, header: int=None,
     ]
     pddf.set_index('Time', inplace=True)
 
-    # preprocessing by interpolating invalid columns
-    if interpolation:
-        for col in pddf.columns:
-            pddf.loc[:, col] = check_nan(pddf[col])
+    # not using numpy for license issue
+    # # preprocessing by interpolating invalid columns
+    # if interpolation:
+        # for col in pddf.columns:
+            # pddf.loc[:, col] = check_nan(pddf[col])
 
-    # calculate the duration of each data point
-    if duration:
-        pddf.loc[:, 'Duration'] = [
-            cal_each_duration(ind, timeind, pddf[pddf.columns[0]])
-            for ind, timeind in enumerate(pddf[pddf.columns[0]].index)
-        ]
+    # # calculate the duration of each data point
+    # if duration:
+        # pddf.loc[:, 'Duration'] = [
+            # cal_each_duration(ind, timeind, pddf[pddf.columns[0]])
+            # for ind, timeind in enumerate(pddf[pddf.columns[0]].index)
+        # ]
 
     return pddf
 
@@ -129,100 +130,100 @@ def interpolate_with_s(mid_date: datetime, a_date: datetime, b_date: datetime,
         (b_date-a_date).seconds+a_value
 
 
-def check_nan(wseries: Series) -> Series:
-    """
-        This function checks the values inside the series. If any of them
-        are nan or str, user interpolation with adjacent values to
-        substitute it. Returns the corrected Series
+# def check_nan(wseries: Series) -> Series:
+    # """
+        # This function checks the values inside the series. If any of them
+        # are nan or str, user interpolation with adjacent values to
+        # substitute it. Returns the corrected Series
 
-        Inputs:
-        ==========
-        wseries: Series
-            pandas Series data with values in float and index as
-            datetime.datetime object
-    """
+        # Inputs:
+        # ==========
+        # wseries: Series
+            # pandas Series data with values in float and index as
+            # datetime.datetime object
+    # """
 
-    if len(wseries[Series([(
-            isinstance(val, str) or isnan(val)
-        ) for val in wseries], index=wseries.index)]) == 0:
-        return wseries  # nothing to change
+    # if len(wseries[Series([(
+            # isinstance(val, str) or isnan(val)
+        # ) for val in wseries], index=wseries.index)]) == 0:
+        # return wseries  # nothing to change
 
-    # ensure that all are either float or nan
-    def _float_or_nan(ent):
-        """
-            Force values to be either a float or nan first
-        """
-        try:
-            return float(ent)
-        except ValueError:
-            return float('nan')
+    # # ensure that all are either float or nan
+    # def _float_or_nan(ent):
+        # """
+            # Force values to be either a float or nan first
+        # """
+        # try:
+            # return float(ent)
+        # except ValueError:
+            # return float('nan')
 
-    wseries = Series(
-        [_float_or_nan(val) for val in wseries], index=wseries.index,
-        name=wseries.name
-    )
+    # wseries = Series(
+        # [_float_or_nan(val) for val in wseries], index=wseries.index,
+        # name=wseries.name
+    # )
 
-    # continue with interpolation or extrapolation if needed
-    inds = where(
-        Series([
-            (isinstance(val, str) or isnan(val)) for val in wseries
-        ], index=wseries.index)
-    )[0]  # locate the position of the problematic readings
-    for ind in inds:
-        try:
-            wseries[ind] = interpolate_with_s(
-                wseries.index[ind], wseries.index[ind-1],
-                wseries.index[ind+1],
-                wseries[ind-1], wseries[ind+1]
-            )
-            if isnan(wseries[ind]):  # interpolation does not work
-                wseries[ind] = interpolate_with_s(
-                    wseries.index[ind], wseries.index[ind-2],
-                    wseries.index[ind-1],
-                    wseries[ind-2], wseries[ind-1]
-                )
-        except IndexError:  # extrapolation
-            try:
-                wseries[ind] = interpolate_with_s(
-                    wseries.index[ind], wseries.index[ind-2],
-                    wseries.index[ind-1],
-                    wseries[ind-2], wseries[ind-1]
-                )
-            except IndexError:
-                wseries[ind] = interpolate_with_s(
-                    wseries.index[ind], wseries.index[ind+2],
-                    wseries.index[ind+1],
-                    wseries[ind+2], wseries[ind+1]
-                )
+    # # continue with interpolation or extrapolation if needed
+    # inds = where(
+        # Series([
+            # (isinstance(val, str) or isnan(val)) for val in wseries
+        # ], index=wseries.index)
+    # )[0]  # locate the position of the problematic readings
+    # for ind in inds:
+        # try:
+            # wseries[ind] = interpolate_with_s(
+                # wseries.index[ind], wseries.index[ind-1],
+                # wseries.index[ind+1],
+                # wseries[ind-1], wseries[ind+1]
+            # )
+            # if isnan(wseries[ind]):  # interpolation does not work
+                # wseries[ind] = interpolate_with_s(
+                    # wseries.index[ind], wseries.index[ind-2],
+                    # wseries.index[ind-1],
+                    # wseries[ind-2], wseries[ind-1]
+                # )
+        # except IndexError:  # extrapolation
+            # try:
+                # wseries[ind] = interpolate_with_s(
+                    # wseries.index[ind], wseries.index[ind-2],
+                    # wseries.index[ind-1],
+                    # wseries[ind-2], wseries[ind-1]
+                # )
+            # except IndexError:
+                # wseries[ind] = interpolate_with_s(
+                    # wseries.index[ind], wseries.index[ind+2],
+                    # wseries.index[ind+1],
+                    # wseries[ind+2], wseries[ind+1]
+                # )
 
-    return wseries
+    # return wseries
 
 
-def cal_each_duration(ind: int, timeind: Timestamp, wseries: Series) -> float:
-    """
-        This function calculates the duration for each data point in the time
-        series given by the acqusition interval of the data
+# def cal_each_duration(ind: int, timeind: Timestamp, wseries: Series) -> float:
+    # """
+        # This function calculates the duration for each data point in the time
+        # series given by the acqusition interval of the data
 
-        Inputs:
-        ==========
-        ind: int
-            index number in the pandas Series
+        # Inputs:
+        # ==========
+        # ind: int
+            # index number in the pandas Series
 
-        timeind: tslib.Timestamp
-            time stamp at the index being analyzed
+        # timeind: tslib.Timestamp
+            # time stamp at the index being analyzed
 
-        wseries: Series
-            pandas Series data with values in float and index as
-            datetime.datetime object
-    """
+        # wseries: Series
+            # pandas Series data with values in float and index as
+            # datetime.datetime object
+    # """
 
-    length = len(wseries)
-    if ind == 0:
-        return (wseries.index[ind+1]-timeind).seconds
-    elif ind == length-1:
-        return (timeind-wseries.index[ind-1]).seconds
-    else:
-        return ((wseries.index[ind+1]-wseries.index[ind-1]).seconds)/2.0
+    # length = len(wseries)
+    # if ind == 0:
+        # return (wseries.index[ind+1]-timeind).seconds
+    # elif ind == length-1:
+        # return (timeind-wseries.index[ind-1]).seconds
+    # else:
+        # return ((wseries.index[ind+1]-wseries.index[ind-1]).seconds)/2.0
 
 
 # testing functions
@@ -230,14 +231,14 @@ if __name__ == '__main__':
 
     from os.path import basename
 
-    FILENAME = '../dat/fixed_time_interval.csv'
-    print('Testing file import by using ', FILENAME)
-    TEST_DF = read_data(FILENAME, header=None, duration=True,
-                        interpolation=True)
-    assert isinstance(TEST_DF.index[0], Timestamp)
-    assert TEST_DF.loc[TEST_DF.index[0], 'Duration'] == 60*30
-    assert TEST_DF.loc[TEST_DF.index[2], 'Duration'] == 60*30
-    assert TEST_DF.loc[TEST_DF.index[-1], 'Duration'] == 60*30
+    # FILENAME = '../dat/fixed_time_interval.csv'
+    # print('Testing file import by using ', FILENAME)
+    # TEST_DF = read_data(FILENAME, header=None, duration=True,
+                        # interpolation=True)
+    # assert isinstance(TEST_DF.index[0], Timestamp)
+    # assert TEST_DF.loc[TEST_DF.index[0], 'Duration'] == 60*30
+    # assert TEST_DF.loc[TEST_DF.index[2], 'Duration'] == 60*30
+    # assert TEST_DF.loc[TEST_DF.index[-1], 'Duration'] == 60*30
 
     FILENAME = '../dat/time_of_change.csv'
     print('Testing file import by using ', FILENAME)
