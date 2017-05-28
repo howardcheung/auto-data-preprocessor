6caa64414a7a66b5828f9c7395e16e73d478b304
howardcheung <howard.at@gmail.com>
2017-05-28 12:26:31 +0800

    Change version number

--- src/gui_main.py
+++ src/gui_main.py
@@ -652,7 +652,7 @@ class MainGUI(wx.Frame):
 
         info = adv.AboutDialogInfo()
         info.SetName('Data Preprocessing Helper')
-        info.SetVersion('0.3.2')
+        info.SetVersion('0.3.3')
         info.SetDescription(DESCRIPTION)
         info.SetCopyright('(C) Copyright 2017 Howard Cheung')
         info.SetWebSite(
