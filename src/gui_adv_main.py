15a1e076709cfafd17d4e9383917bb392e68556c
howardcheung <howard.at@gmail.com>
2017-08-29 22:44:34 +0800

    Create template of advanced tab

---
+++ src/gui_adv_main.py
@@ -0,0 +1,110 @@
+﻿#!/usr/bin/python3
+"""
+    This script file contains methods to define the graphical user interface
+    of the tool with basic setting for beginners and advanced setting for
+    flexibility
+
+    Author: Howard Cheung (howard.at@gmail.com)
+    Date: 2017/08/29
+    License of the source code: MIT license
+"""
+
+# import python internal modules
+from calendar import monthrange
+from datetime import datetime
+from math import isnan
+from ntpath import split
+from os.path import isfile, dirname
+from pathlib import Path
+from traceback import format_exc
+from webbrowser import open as webbrowseropen
+
+# import third party modules
+from pandas import ExcelFile
+import wx
+from wx import adv
+
+# import user-defined modules
+from data_read import read_data
+from format_data import convert_df
+
+
+# define global variables
+DESCRIPTION = \
+"""Data Preprocessing Helper is a software made to
+help people to preprocess their data that contain ugly
+features such as time-of-change values, blank values, etc
+to nicely presented data with no blank values
+"""
+LICENSE = \
+"""GNU GENERAL PUBLIC LICENSE Version 3
+Data Preprocessing Helper
+Copyright (C) 2017 Howard Cheung
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+For licenses of modules involved in the development of the software,
+please visit <https://github.com/howardcheung/data-preprocessing-helper/>
+"""
+
+# Some classes to use for the notebook pages.  Obviously you would
+# want to use something more meaningful for your application, these
+# are just for illustration.
+
+class PageOne(wx.Panel):
+    def __init__(self, parent):
+        wx.Panel.__init__(self, parent)
+        t = wx.StaticText(self, -1, "This is a PageOne object", (20,20))
+
+class PageTwo(wx.Panel):
+    def __init__(self, parent):
+        wx.Panel.__init__(self, parent)
+        t = wx.StaticText(self, -1, "This is a PageTwo object", (40,40))
+
+
+class MainFrame(wx.Frame):
+    def __init__(self):
+        wx.Frame.__init__(self, None, title="Simple Notebook Example")
+
+        # Here we create a panel and a notebook on the panel
+        p = wx.Panel(self)
+        nb = wx.Notebook(p)
+
+        # create the page windows as children of the notebook
+        page1 = PageOne(nb)
+        page2 = PageTwo(nb)
+
+        # add the pages to the notebook with the label to show on the tab
+        nb.AddPage(page1, "Page 1")
+        nb.AddPage(page2, "Page 2")
+
+        # finally, put the notebook in a sizer for the panel to manage
+        # the layout
+        sizer = wx.BoxSizer()
+        sizer.Add(nb, 1, wx.EXPAND)
+        p.SetSizer(sizer)
+
+
+# define functions
+def gui_main():
+    """
+        Main function to intiate the GUI
+    """
+    app = wx.App()
+    MainFrame().Show()
+    app.MainLoop()
+
+
+if __name__ == "__main__":
+    gui_main()
